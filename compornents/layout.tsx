9b762cff6768d87489a056e4c2837754aef204db
Kentaro Takata <ken.takata001@gmail.com>
2021-06-08 14:21:12 +0900

    chore

--- compornents/layout.tsx
+++ compornents/layout.tsx
@@ -1,13 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
 
-const name = 'Kentaro Takata'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children }: React.PropsWithChildren<{}>) {
+
+
   return (
     <div className={styles.container}>
       <Head>
