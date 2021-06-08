2ad1380acb7c7cab6b0761be4055cbf992f15da0
Kentaro Takata <ken.takata001@gmail.com>
2021-06-08 14:21:40 +0900

    feat:検索ボタンを押すと検索中のキーワードが表示される

--- pages/recomend.tsx
+++ pages/recomend.tsx
@@ -1,9 +1,14 @@
-import Link from 'next/link'
+import { useState } from "react";
 import Head from 'next/head'
 import Layout from '../compornents/layout'
-import styles from '../styles/recomend.module.css'
 
 export default function FirstPost() {
+  const [keyword, setKeyword] = useState("");
+  const [viewKeyword, setViewKeyword] = useState("");
+
+  const searchTech = () => {
+    setViewKeyword(keyword);
+  };
   return (
     <Layout>
       <Head>
@@ -58,12 +63,12 @@ export default function FirstPost() {
       <h2 className="container mx-auto mt-32 flex justify-center text-5xl">技術キーワード検索</h2>
       <div className="container mx-auto m-2 flex justify-center">
         <form>
-          <input type="text" className="m-2 border border-transparent focus:outline-none focus:ring-2 focus:ring-green-600 focus:border-transparent py-2 px-4 font-semibold rounded-lg shadow-md text-black" />
-          <button className="m-2 bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-opacity-50 py-2 px-4 font-semibold rounded-lg shadow-md text-white">
-            検索
-          </button>
+          <input type="text" value={keyword} onChange={e => setKeyword(e.target.value)}
+className="m-2 border border-transparent focus:outline-none focus:ring-2 focus:ring-green-600 focus:border-transparent py-2 px-4 font-semibold rounded-lg shadow-md text-black" />
+          <input type="button" value="検索" onClick={() => searchTech()} className="m-2 bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-opacity-50 py-2 px-4 font-semibold rounded-lg shadow-md text-white cursor-pointer"/>
         </form>
       </div>
+      <div className="container mx-auto m-2 flex justify-center">{`検索キーワード:${viewKeyword}`}</div>
         <div className="container mx-auto m-2 grid grid-cols-3 gap-4">
         <div className="p-3 rounded-xl border border-black bg-white shadow-md">
               <section className="text-3xl font-bold">
